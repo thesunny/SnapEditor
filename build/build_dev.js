375fd49e7d5d5fe4801e014464fd5618c78abfde
Wesley Wong <wesley@snapeditor.com>
2013-08-21 09:35:21 -0700

    Adds #248 - Rework image uploads
    Includes jQuery File Upload.

--- build/build_dev.js
+++ build/build_dev.js
@@ -10,5 +10,19 @@
   wrap: {
     startFile: "start.frag",
     end: "}());"
+  },
+  paths: {
+    "jquery.ui.widget": "../lib/jQuery-File-Upload/js/vendor/jquery.ui.widget",
+    "jquery.iframe-transport": "../lib/jQuery-File-Upload/js/jquery.iframe-transport",
+    "jquery.fileupload": "../lib/jQuery-File-Upload/js/jquery.fileupload",
+    "jquery.fileupload-image": "../lib/jQuery-File-Upload/js/jquery.fileupload-image",
+    "jquery.fileupload-process": "../lib/jQuery-File-Upload/js/jquery.fileupload-process",
+    "load-image": "../lib/JavaScript-Load-Image/js/load-image",
+    "load-image-meta": "../lib/JavaScript-Load-Image/js/load-image-meta",
+    "load-image-exif": "../lib/JavaScript-Load-Image/js/load-image-exif",
+    //"load-image-exif-map": "../lib/JavaScript-Load-Image/js/load-image-exif-map",
+    "load-image-ios": "../lib/JavaScript-Load-Image/js/load-image-ios",
+    //"load-image-orientation": "../lib/JavaScript-Load-Image/js/load-image-orientation",
+    "canvas-to-blob": "../lib/JavaScript-Canvas-to-Blob/js/canvas-to-blob"
   }
 })
