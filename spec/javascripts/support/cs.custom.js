7a25578a520698f03314838b8863baa8a0724d1c
Wesley Wong <wesley@snapeditor.com>
2013-09-23 11:05:34 -0700

    More license headers.

--- spec/javascripts/support/cs.custom.js
+++ spec/javascripts/support/cs.custom.js
@@ -1,3 +1,7 @@
+/*
+Copyright (c) 2012-2013 8098182 Canada Inc. All rights reserved.
+For licensing, see COPYING and COPYING.LESSER.
+*/
 // cs.js loads all modules asynchronously. The following are all copied
 // directly from cs.js, but with modifications so that the modules are loaded
 // synchronously.
